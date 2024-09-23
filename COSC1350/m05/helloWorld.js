5faccd37073429ba27d78cab43c2dfa0f51d7db7
Estallin1470 <emileystallings@westernwyoming.edu>
2024-09-23 17:41:15 -0600

    Add files via upload

---
+++ COSC1350/m05/helloWorld.js
@@ -0,0 +1,8 @@
+/*
+    Emiley Stallings
+    helloWorld.js
+    09/23/24
+*/
+
+let hello = "Hello World";
+console.log(hello);
---
+++ COSC1350/m05/practiceVariables.js
@@ -0,0 +1,26 @@
+/*
+    Emiley Stallings
+    practiceVariables.js
+    09/23/24
+*/
+let hiDrT = "Hi, Dr. T!";
+let numFloat = 4.5;
+let zero = 0;
+let numInt = -10;
+let isTrue = true;
+
+console.log(typeof hiDrT);
+console.log(typeof numFloat);
+console.log(typeof zero);
+console.log(typeof numInt);
+console.log(typeof isTrue);
+
+
+console.log("Hi Dr. T!\nLet's get started!");
+console.log(`half of 100 is ${100 / 2}`);
+console.log("con" + "cat" + "e" + "nate");
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == true);
